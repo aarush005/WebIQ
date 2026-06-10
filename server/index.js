5de1ed3011e6a2351ef862097b8b51c6f1194291
aarush005 <astaarush005@gmail.com>
2026-06-10 18:39:03 +0530

    Fix ES module in server

--- server/index.js
+++ server/index.js
@@ -1,37 +1,24 @@
-
-// load environment variables from .env file
-require("dotenv").config();
-
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
+// import auditRoutes from "./routes/audit.js";
+// import stripeRoutes from "./routes/stripe.js";
 
-const auditRoutes = require("./routes/audit")
-const stripeRoutes = require("./routes/stripe")
-
-const app = expres()
+const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware 
-
 app.use(cors({
-    origin: ["http://localhost:5173", "https://webiq.vercel.app"],
-}))
+  origin: ["http://localhost:5173", "https://growthlens.vercel.app"],
+}));
 app.use(express.json());
 
-// Routes 
+// app.use("/api/audit", auditRoutes);
+// app.use("/api/stripe", stripeRoutes);
 
-app.use("/api/audit", auditRoutes);
-app.use("/api/stripe", stripeRoutes);
-
-
-// Health check 
-
-app.get("/health", (req,res)=>{
-    res.json({
-        status: "ok", time: new Date().toISOString()
-    })
-})
+app.get("/health", (req, res) => {
+  res.json({ status: "ok", time: new Date().toISOString() });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on http:localhost:${PORT}`);
-})
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
